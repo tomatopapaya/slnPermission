8dab8ac2210b50da01eade55c83dd49f745621d4
03386 <mochidanny618@gmail.com>
2023-12-06 09:45:40 +0800

    將SEPMIS_NEW傳送到GitHub

---
+++ prjPermission/obj/Release/Package/PackageTmp/Scripts/TreeView.js
@@ -0,0 +1,34 @@
+﻿$(function () {
+    $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', '收起');
+    $('.tree li.parent_li > span').on('click', function (e) {
+        var children = $(this).parent('li.parent_li').find(' > ul > li');
+        if (children.is(":visible")) {
+            children.hide('fast');
+            $(this).attr('title', '展開').find(' > i').addClass('fa-plus-square').removeClass('fa-minus-square');
+        } else {
+            children.show('fast');
+            $(this).attr('title', '收起').find(' > i').addClass('fa-minus-square').removeClass('fa-plus-square');
+        }
+        e.stopPropagation();
+    });
+});
+
+function TreeViewClick(clicked_id) {
+    url = '@Url.Action("TreeVieww", "Dept")'; //Url.Action(actionName, ControllerName)
+    $.ajax({
+        url: url,
+        data: { id: clicked_id }, //json format
+        success: function ShowList(returnData) {
+/*            alert(clicked_id);*/
+        },
+        error: {
+
+        }
+    });
+}
+
+
+
+
+
+
---
+++ prjPermission/obj/Release/Package/PackageTmp/Scripts/dept_mgmt.js
@@ -0,0 +1,36 @@
+﻿function TreeViewClick(clicked_id) {
+    $.ajax({
+        url: '/Dept/TreeVieww',
+        type: 'post',
+        dataType: 'json',
+        /*            contentType: 'application/json; charset=utf-8',*/
+        data: { deptno: clicked_id }, //json format
+
+        success: function (returnData) {
+            //部門資訊
+            $('#dept').empty();
+            $("#dept").append("<th>部門編號	</th><th>部門名稱</th><th>經管部門</th><th>部門主管</th>");
+            for (var i = 0; i < 1; i++) {
+                $("#dept").append(
+                    "<tr>" +
+                    "<td>" + returnData[i].DEPT_NO + "</td>" +
+                    "<td>" + returnData[i].DEPT_NAME + "</td>" +
+                    "<td>" + returnData[i].DEPT_CHIEF + "</td>" +
+                    "<td>" + returnData[i].MGR_FG + "</td>" +
+                    "</tr>"
+                );
+            }
+            //部門使用者
+            $('#user').empty();
+            $("#user").append("<th>使用者編號</th><th>使用者名稱</th><th>處理</th>")
+            for (var i = 0; i < returnData.length; i++) {
+                $("#user").append(
+                    "<tr>" +
+                    "<td>" + returnData[i].USR_ID + "</td>" +
+                    "<td>" + returnData[i].USR_NAME + "</td>" +
+                    "</tr>"
+                );
+            }
+        }
+    })
+}
